e9b271ac402c6e0a50a6b293ab76737c711d3ec0
belal <belal20795@gmail.com>
2020-05-07 22:08:33 +0530

    added a simple addition

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
-app.get ('/', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+
+
+app.get ("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+
+
+app.post ("/", (req, res) => {
+    
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+
+    let result = num1 + num2 ;
+
+    console.log(req.body.num1);
+    res.send("addition is " + result);
+})
+
+
 
 app.listen(3000, () => console.log('running on port 3000'))
 
